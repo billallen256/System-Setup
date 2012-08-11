fcb5bf9574629098c7623df57149e5ab8d03a1a7
Bill Allen <photo.allen@gmail.com>
2012-08-11 12:42:38 -0400

    Updated for Virtual Box 4.1

--- Ubuntu/virtual-box.sh
+++ Ubuntu/virtual-box.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -f /media/VboxLinuxAdditions.amd64.run ];
+if [ ! -f /media/VBOXADDITIONS*/VBoxLinuxAdditions.run ];
 then
-	echo Make sure Guest Additions is mounted before running this script.
+	echo Make sure Virtual Box Additions is mounted before running this script.
 	exit 1
 fi
 
 sudo apt-get install build-essential dkms
-sudo /media/VboxLinuxAdditions-amd64.run
+sudo /media/VBOXADDITIONS*/VBoxLinuxAdditions.run
