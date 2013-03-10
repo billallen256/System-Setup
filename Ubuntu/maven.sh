47cfb69fbc90645a0eacd4c0dfea3826eedce441
Bill Allen <photo.allen@gmail.com>
2013-03-10 19:54:56 -0400

    Some minor bugs.

--- Ubuntu/maven.sh
+++ Ubuntu/maven.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-pushd /tmp
+pushd ~/tmp
 wget http://mirrors.ibiblio.org/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 tar xzvf apache-maven-3.0.5-bin.tar.gz
-mv apache-maven-3.0.5 /opt/
-cp maven/maven.sh /etc/profile.d/
-source /etc/profile.d/maven.sh
+sudo mv apache-maven-3.0.5 /opt/
 popd
+
+sudo cp maven/maven.sh /etc/profile.d/
+source /etc/profile.d/maven.sh
+sudo update-alternatives --install "/usr/bin/mvn" "mvn" "/opt/apache-maven-3.0.5/bin/mvn" 1
