00a161fc3b86445d8bfcc2c4678c74ef9d252d42
Bill Allen <photo.allen@gmail.com>
2014-03-07 10:36:02 -0700

    golang syntax highlighting now works.

--- Ubuntu/install/golang.sh
+++ Ubuntu/install/golang.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install -y golang vim-syntax-go
+
+# thanks to http://collectingknowledge.blogspot.com/2013/02/using-go-in-ubuntu-1204.html
+mkdir -p ~/.vim
+mkdir -p ~/.vim/syntax
+mkdir -p ~/.vim/ftdetect
+cp /usr/share/vim/addons/syntax/go*.vim ~/.vim/syntax
+echo "au BufRead,BufNewFile *.go set filetype=go" > ~/.vim/ftdetect/go.vim
