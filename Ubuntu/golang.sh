666cdd10552eccafd9b078cd1618294b4a40b72e
Bill Allen <photo.allen@gmail.com>
2014-03-07 09:55:02 -0700

    Added a script to install golang, and added -y.

---
+++ Ubuntu/golang.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install -y golang vim-syntax-go
--- Ubuntu/init-pkgs.sh
+++ Ubuntu/init-pkgs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install build-essential dkms nedit git-core git-gui git-doc vim
+sudo apt-get install -y build-essential dkms nedit git-core git-gui git-doc vim
