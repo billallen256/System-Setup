199dedcf90aaf18dde312566323dc1edc3c7281e
Bill Allen <photo.allen@gmail.com>
2014-05-11 07:55:37 -0600

    Fixing the installer since Ubuntu wants to use Go 1.

--- Ubuntu/install/golang.sh
+++ Ubuntu/install/golang.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install -y golang vim-syntax-go
+mkdir -p ~/tmp
+cd ~/tmp
+wget https://storage.googleapis.com/golang/go1.2.2.linux-amd64.tar.gz
+tar xzvf go1.2.2.linux-amd64.tar.gz
+sudo mv ~/tmp/go /usr/lib/
+sudo chown -R root:root /usr/lib/go
+sudo su - -c "echo \"export GOROOT=/usr/lib/go\" > /etc/profile.d/go.sh"
+source /etc/profile.d/go.sh
+sudo ln -s /usr/lib/go/bin/go /usr/bin/go
+sudo ln -s /usr/lib/go/bin/godoc /usr/bin/godoc
+sudo ln -s /usr/lib/go/bin/gofmt /usr/bin/gofmt
+sudo apt-get install -y vim-syntax-go
 
 # thanks to http://collectingknowledge.blogspot.com/2013/02/using-go-in-ubuntu-1204.html
 mkdir -p ~/.vim
