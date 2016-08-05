08b98bb5710c05305032a5f65869e6ac1f3ae7f9
Bill Allen <photo.allen@gmail.com>
2016-08-05 13:45:34 -0600

    Some updates from a recent Ubuntu 16.04 install.

--- Ubuntu/install/init-pkgs.sh
+++ Ubuntu/install/init-pkgs.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-sudo apt-get install -y build-essential dkms nedit git-core git-gui git-doc vim mercurial
+mkdir -p ~/tmp
+
+sudo apt install -y git vim wget
+
+pushd ~/tmp
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub 
+popd
+
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+sudo apt install -y unity-tweak-tool
+sudo apt install -y gnome-session-flashback
