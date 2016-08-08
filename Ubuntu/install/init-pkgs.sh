11bf16554f9ed605ddcb9766089faa170a022be9
Bill Allen <photo.allen@gmail.com>
2016-08-07 20:26:42 -0600

    Forgot to add the new repo.

--- Ubuntu/install/init-pkgs.sh
+++ Ubuntu/install/init-pkgs.sh
@@ -9,6 +9,7 @@ wget https://dl.google.com/linux/linux_signing_key.pub
 sudo apt-key add linux_signing_key.pub 
 popd
 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt update
 sudo apt install -y google-chrome-stable
 
